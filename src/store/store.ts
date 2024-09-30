156b927c14602a8a8b00d8d34232fe4699f97c56
Arqwels <danil.kuzmin.1994@gmail.com>
2024-10-01 01:26:33 +0400

    Удалил ненужный файл

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { nearApi } from "./api/nearApi";
-import { walletsApi } from "./api/walletsApi";
 import { addressApi } from "./api/adressApi";
 
 const store = configureStore({
   reducer: {
     [addressApi.reducerPath]: addressApi.reducer,
     [nearApi.reducerPath]: nearApi.reducer,
-    [walletsApi.reducerPath]: walletsApi.reducer,
   },
   middleware: (getDefaultMiddleware) => 
-    getDefaultMiddleware().concat(addressApi.middleware, nearApi.middleware, walletsApi.middleware),
+    getDefaultMiddleware().concat(addressApi.middleware, nearApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
